4c7b959ab3909a2efbb265b1027d9d584ed0e0bd
leannep <leanne@lsst.org>
2026-07-23 11:44:18 +0200

    Exclude RTN-115.tex from copyedit checks

--- bin/copyedit.py
+++ bin/copyedit.py
@@ -28,7 +28,7 @@ sys.path.insert(0, str(Path(__file__).parent.parent / "python"))
 
 from lsst.dptwo.utils.copyedit import load_rules, process_file
 
-_EXCLUDE = re.compile(r"^(lsst-texmf/|aglossary\.tex$|tests/data/)")
+_EXCLUDE = re.compile(r"^(lsst-texmf/|aglossary\.tex$|tests/data/|RTN-115\.tex$)")
 
 
 def _all_tex_files() -> list[str]:
